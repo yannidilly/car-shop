40f2bd71df573cb5f37065602edd925c7fd53f58
Yanni <yannidilly.prog@gmail.com>
2023-05-01 19:37:14 -0300

    cria classe domain Motorcycle

---
+++ src/Interfaces/IMotorcycle.ts
@@ -0,0 +1,12 @@
+interface IVehicle {
+  id?: string | undefined;
+  model: string;
+  year: number;
+  color: string;
+  status?: boolean;
+  buyValue: number;
+  category: string;
+  engineCapacity: number;
+}
+
+export default IVehicle;
